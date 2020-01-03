5d9f35a962716d6c18f23605fe2fa536b4b1f2bb
Zulifqar Ahmad <zulifqar.ahmad@hotmail.com>
2020-01-03 17:01:09 -0600

    ravens class four

---
+++ ravens/class-four/class.four.js
@@ -0,0 +1,10 @@
+function changeColor(newColor) {
+  var elem = document.getElementById('para');
+  elem.style.color = newColor;
+}
+
+function testLogs() {
+  console.log(23);
+  console.info('all is good , just info');
+  console.warn('....2020, trump is winning !');
+}
