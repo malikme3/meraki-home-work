3abed42f8d61c00f96d5d076e31904f2bde0aedf
Zulifqar Ahmad <zulifqar.ahmad@hotmail.com>
2020-01-05 17:05:02 -0600

    week four assignment

---
+++ ravens/class-four/class-four-assignment.js
@@ -0,0 +1,14 @@
+(function() {
+  // Result for
+  console.log('********* Firs group result *********');
+  console.log('Ali');
+  console.log('Hussain');
+  console.log('********* Second group result *********');
+  console.warn('Yasir');
+  console.warn('Khan');
+  console.log('********* Thirs group result *********');
+  console.info('Wasim');
+
+  console.log('********* Fourth group result *********');
+  console.log('Wasim Akram Kahn');
+})();
